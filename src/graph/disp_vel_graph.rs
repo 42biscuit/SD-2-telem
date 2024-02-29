01eda422a6d32b1f069942f4ba89f80d13f820e8
William Wood <wwood1@sheffield.ac.uk>
2024-02-29 17:25:21 +0000

    added disp_vel_graph.rs

---
+++ src/graph/disp_vel_graph.rs
@@ -0,0 +1,16 @@
+use crate::data::TelemData;
+use egui::Id;
+use egui_plot::{Plot, PlotPoint};
+use egui_plot::{Bar, BarChart};
+
+use super::Graph;
+
+
+pub struct DispVelGraph{
+    data : Vec<PlotPoint>,
+
+}
+
+impl DispVelGraph {
+    
+}   
